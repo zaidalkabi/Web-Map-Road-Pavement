95b077773b0f1b9ef3e330b96640c16a6ddef23c
zaidalkabi <91412132+zaidalkabi@users.noreply.github.com>
2024-06-29 11:32:20 +0300

    Add files via upload

---
+++ data/Photos_1.js
@@ -0,0 +1,14 @@
+var json_Photos_1 = {
+"type": "FeatureCollection",
+"name": "Photos_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\Alligator cracking.jpg", "filename": "Alligator cracking", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.46145536111111", "latitude": "33.33707180555556", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 44.461455361111113, 33.33707180555556 ] } },
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\gps video.jpeg", "filename": "gps video", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.465354000000005", "latitude": "33.350346", "timestamp": "2024-06-07T15:02:16.000" }, "geometry": { "type": "Point", "coordinates": [ 44.465354, 33.350346 ] } },
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\pothole.jpg", "filename": "pothole", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.44913863888888", "latitude": "33.30227778888889", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 44.449138638888883, 33.302277788888887 ] } },
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\pothole1.jpg", "filename": "pothole1", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.435191149999994", "latitude": "33.32063133333334", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 44.43519115, 33.320631333333338 ] } },
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\rutting 3.jpg", "filename": "rutting 3", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.37854288888889", "latitude": "33.238508055555556", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 44.378542888888887, 33.238508055555556 ] } },
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\rutting.jpeg", "filename": "rutting", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.42418336111111", "latitude": "33.2480195", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 44.424183361111112, 33.2480195 ] } },
+{ "type": "Feature", "properties": { "photo": "D:\\images\\web map\\rutting2.jpg", "filename": "rutting2", "directory": "D:\\images\\web map", "altitude": null, "direction": null, "longitude": "44.44519041666666", "latitude": "33.28289794444444", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 44.445190416666662, 33.282897944444443 ] } }
+]
+}
